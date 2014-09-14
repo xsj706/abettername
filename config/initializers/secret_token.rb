e00a0a7cd9be7e18cf514f3a8f3a1b5108d15ae4
Car Shaker <xsj706@gmail.com>
2014-09-13 17:25:25 -0700

    intiial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatthefuck::Application.config.secret_token = '406d6b6521c32020c9d1b310dce19939245de32610755bbe6f7111f098212d07beb358ed399c64a9d74d75378f8f51096b47d5c97fd4ee522761a7481d855f3c'
